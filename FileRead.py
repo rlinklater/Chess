cde63c25a5de4b6ce0d27959d15cf4a05280a4b2
DanielStreb1992 <Danielstreb@csu.fullerton.edu>
2015-09-30 12:18:23 -0700

    FileRead.py
    Still need modification

---
+++ FileRead.py
@@ -0,0 +1,79 @@
+# Function to take in a text file
+# with coordinates in the format
+# x.K(x, y) 			// For PlayerX King
+# x.R(x, y)			// For PlayerX Rook
+# y.K(x, y) 			// For PlayerY King
+
+Debug = True
+
+Workfile = "testCase.txt"
+
+def FileRead():
+	Coordinates = []
+	with open(workfile, 'r') as f:
+		if Debug:
+			print("{} successfully opened".format(Workfile))
+		
+		# Loop to iterate through each line in the provided text file
+		# Assumes lines in the form: p.cp(x,y)		where p is the character
+		# representing the player(x, y), cp is the character representing the
+		# chess piece(K for king, R for rook), and (x,y) are the coordinates 
+		# of the chess piece on the chess board(1-8).
+		for line in f:
+			if Debug:
+				print(line)
+			if line[0] == "x":
+				# PlayerX piece coordinate
+				if line[2].upper() == "K":
+					# PlayerX - King Piece
+					if validCoord(line):
+						xK = (line[4], line[6]) # Creates tuple for PlayerX's King
+						Coordinates.append(xK)
+				elif line[2].upper() == "R":
+					#		PlayerX - Rook Piece
+					if validCoord(line):
+						xR = (line[4], line[6])	# Creates tuple for PlayerX's Rook
+						Coordinates.append(xR)	
+				else:
+					# Print "Unrecognized piece character"
+					pass	
+			elif line[0] == "y":
+				# PlayerY piece coordinate
+				if line[2].upper() != "K":
+					# Print "Unrecognized piece chacter"
+					pass
+				else:
+					# PlayerY - King Piece
+					if validCoord(line):
+						yK = (line[4], line[6]) #	Creates tuple for PlayerY's King
+						Coordinates.append(yK)
+			else:
+				# Print "Coordinate format unrecognized"
+				pass
+
+
+	# For Debugging purposes
+	if Debug:
+		if f.closed:
+			print('File closed successfully')
+		else:
+			print('File was not closed successfully')
+		print(Coordinates)
+		
+	return Coordinates
+
+# Helper Function
+# Checks if provided coordinate 
+# falls within a legal chessboard
+# range: 8X8		
+def validCoord(l):
+	if ((int(l[4])	>= 1) and (int(l[4]) <= 8)) and ((int(l[6])	>= 1) and (int(l[6]) <= 8)):
+		return True # Coordinate is valid
+	else:
+		print("Coordinate out of range(1-8)")
+		return False # Coordinate is invalid
+		
+		
+# Tuple:		("x", "r", 1, 3)
+
+
